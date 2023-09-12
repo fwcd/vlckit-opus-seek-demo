b956d0cddbc535529f25e263b61dd40e7807d458
fwcd <fwcdmail@gmail.com>
2023-09-12 16:46:35 +0200

    Set up demo app

---
+++ VLCKitOpusSeekDemo/VLCKitOpusSeekDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VLCKitOpusSeekDemoApp.swift
+//  VLCKitOpusSeekDemo
+//
+//  Created on 12.09.23
+//
+
+import SwiftUI
+
+@main
+struct VLCKitOpusSeekDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
